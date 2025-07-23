305e65870cfd6e6491a49612aab3f22593ed40e5
Kukyeon <rnrdus2000@naver.com>
2025-07-23 10:03:24 +0900

    주석춛가

--- src/classEx_0722/Score.java
+++ src/classEx_0722/Score.java
@@ -9,11 +9,11 @@ public class Score {
 	int math;
 
 	public int printScore() {
-		int score = kor + eng + math; //총점
+		int score = kor + eng + math; //총점 this. 사용해도 괜츈
 		return score;
 	}	
 	
-	
+	// double score1 = score / 3.0 도 가능
 	public double printScore1() {
 		double score1 = (kor + eng + math) / 3; //평균
 		return score1;
